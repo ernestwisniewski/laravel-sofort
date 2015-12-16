5a7ae84304cccb2f1151299174c92293573bcfd9
Ernest Wisniewski <ernest@netkata.com>
2015-12-16 13:48:44 +0100

    Rest api

--- src/Sofort/Sofort.php
+++ src/Sofort/Sofort.php
@@ -1,8 +1,16 @@
 <?php namespace ErnestWisniewski\LaravelSofort;
 
 use Sofort\SofortLib\Billcode;
+use Sofort\SofortLib\BillcodeDetails;
+use Sofort\SofortLib\Notification;
+use Sofort\SofortLib\Paycode;
+use Sofort\SofortLib\PaycodeDetails;
+use Sofort\SofortLib\Refund;
+use Sofort\SofortLib\Sofortueberweisung;
+use Sofort\SofortLib\TransactionData;
 
-class Sofort {
+class Sofort
+{
 
     /**
      * The private Sofort key
@@ -16,15 +24,110 @@ class Sofort {
      */
     public function __construct($config_key)
     {
-        $this->private_key = $config_key;
+        $this->config_key = $config_key;
     }
 
+    /**
+     * @param null $config_key
+     * @return Billcode
+     */
     public function billcode($config_key = null)
     {
         if (!$config_key) {
-            $config_key = $this->private_key;
+            $config_key = $this->config_key;
         }
 
         return new Billcode($config_key);
+    }
+
+    /**
+     * @param null $config_key
+     * @return BillcodeDetails
+     */
+    public function billcodeDetails($config_key = null)
+    {
+        if (!$config_key) {
+            $config_key = $this->config_key;
+        }
+
+        return new BillcodeDetails($config_key);
+    }
+
+    /**
+     * @param null $config_key
+     * @return Paycode
+     */
+    public function paycode($config_key = null)
+    {
+        if (!$config_key) {
+            $config_key = $this->config_key;
+        }
+
+        return new Paycode($config_key);
+    }
+
+    /**
+     * @param null $config_key
+     * @return PaycodeDetails
+     */
+    public function paycodeDetails($config_key = null)
+    {
+        if (!$config_key) {
+            $config_key = $this->config_key;
+        }
+
+        return new PaycodeDetails($config_key);
+    }
+
+    /**
+     * @param null $config_key
+     * @return Refund
+     */
+    public function refund($config_key = null)
+    {
+        if (!$config_key) {
+            $config_key = $this->config_key;
+        }
+
+        return new Refund($config_key);
+    }
+
+    /**
+     * @param null $config_key
+     * @return Sofortueberweisung
+     */
+    public function sofortueberweisung($config_key = null)
+    {
+        if (!$config_key) {
+            $config_key = $this->config_key;
+        }
+
+        return new Sofortueberweisung($config_key);
+    }
+
+    /**
+     * @param null $config_key
+     * @return Notification
+     */
+    public function notification($config_key = null)
+    {
+        if (!$config_key) {
+            $config_key = $this->config_key;
+        }
+
+        return new Notification($config_key);
+    }
+
+    /**
+     * @param null $config_key
+     * @return TransactionData
+     */
+    public function transactionData($config_key = null)
+    {
+        if (!$config_key) {
+            $config_key = $this->config_key;
+        }
+
+        return new TransactionData($config_key);
     }
 }
